f9dbcf6ce810a7ce695c0e6027731e1e32a37bff
Roberto Panetta <rob.pane@gmail.com>
2018-09-26 18:59:25 -0400

    MyDate

---
+++ MyDate/src/MyDate.java
@@ -0,0 +1,21 @@
+
+public class MyDate {
+	
+	private int year;
+	private int month;
+	private int day;
+	
+	
+	public MyDate() {
+		
+	}
+	
+	
+	public MyDate(int year, int month, int day) {
+		super();
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}	
+
+}
